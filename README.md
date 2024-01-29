ccbc099ffb200352a0cd235d1284d96241ce6cfa
Lukas Moravec <sydnexpklover@gmail.com>
2024-01-29 22:11:27 +0100

    Write output hists file

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Tree statistics
 
 Rust binary for collecting statistics on trees in bracket notation format.
+
+## Usage:
+
+```bash
+Program with CLI for collectiong statistics on tree collections in bracket notation
+
+Usage: tree-statistics [OPTIONS] --dataset-path <FILE>
+
+Options:
+  -d, --dataset-path <FILE>  Dataset file of trees in bracket notation
+  -q                         outputs only collected statistics
+      --hists                outputs data for degree, leaf paths and labels histograms
+  -h, --help                 Print help
+  -V, --version              Print version
+```
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use std::fmt::Display;
+use std::fs::File;
+use std::io::{BufWriter, Write};
 use clap::Parser;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::process::exit;
 use rayon::prelude::*;
+use crate::statistics::TreeStatistics;
 
 mod parsing;
 mod statistics;
@@ -13,9 +17,15 @@ struct Cli {
     /// Dataset file of trees in bracket notation
     #[arg(short, long, value_name = "FILE")]
     dataset_path: PathBuf,
+    /// outputs only collected statistics
+    #[arg(short, default_value_t = false)]
+    quiet: bool,
+    /// outputs data for degree, leaf paths and labels histograms
+    #[arg(long, default_value_t = false)]
+    hists: bool,
 }
 
-fn main() -> Result<(), clap::Error> {
+fn main() -> Result<(), anyhow::Error> {
     let cli = Cli::parse();
 
     if !cli.dataset_path.exists() || !cli.dataset_path.is_file() {
@@ -30,12 +40,49 @@ fn main() -> Result<(), clap::Error> {
             exit(1);
         }
     };
-    println!("Parsed {} trees", trees.len());
+    if !cli.quiet {
+        println!("Parsed {} trees", trees.len());
+        println!("Gathering statistics");
+    }
 
-    println!("Gathering statistics");
     let stats: Vec<_> = trees.par_iter().map(statistics::gather).collect();
     let summary = statistics::summarize(&stats);
 
     println!("Collection statistics\n{summary}");
+
+    if cli.hists {
+        write_files(&stats)?;
+    }
+    Ok(())
+}
+
+fn write_files(stats: &[TreeStatistics]) -> Result<(), anyhow::Error> {
+    write_file(
+        PathBuf::from("degrees.csv"),
+        &stats.iter().flat_map(|s| &s.degrees).collect::<Vec<&usize>>()
+    )?;
+    write_file(
+        PathBuf::from("depths.csv"),
+        &stats.iter().flat_map(|s| &s.depths).collect::<Vec<&usize>>()
+    )?;
+    write_file(
+        PathBuf::from("labels.csv"),
+        &stats.iter().flat_map(|s| {
+            s.distinct_labels.iter().map(|(k, v)| format!("{k},{v}")).collect::<Vec<_>>()
+        }).collect::<Vec<_>>()
+    )?;
+
+    Ok(())
+}
+
+fn write_file<T, P>(file_name: P, data: &[T]) -> Result<(), std::io::Error>
+    where T: Display,
+    P: AsRef<Path> {
+    let f = File::create(file_name.as_ref().to_path_buf())?;
+    let mut w = BufWriter::new(f);
+
+    for d in data.iter() {
+        writeln!(w, "{d}")?;
+    }
     Ok(())
 }
