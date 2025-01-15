5399714df150d595f3c6e0f2f969909e70376805
Lukas Moravec <sydnexpklover@gmail.com>
2025-01-15 18:01:40 +0100

    Times for dissimilarity

--- dissimilarity_run.sh
+++ dissimilarity_run.sh
@@ -12,4 +12,6 @@ python3 tree_generator.py -T 1000 -D $LBLS -S 0.5 -M 95,105 -B $BASE -X 0.5 > $P
 python3  dissimilarity_query_gen.py -F $P/trees.bracket
 
 ./target/release/tree-statistics --quiet -d $P/trees.bracket lower-bound -q $P/dissimilarity_query.csv --output $P | tail -n 18 > $P/query_times.txt
-./resources/query_validate $P/trees.bracket $P/dissimilarity_query.csv $P/Structural_candidates.csv topdiff > $P/verified.txt
+./resources/query_validate $P/trees.bracket $P/dissimilarity_query.csv $P/Lblint_candidates.csv topdiff > $P/lblint-verified.txt 2> /dev/null
+./resources/query_validate $P/trees.bracket $P/dissimilarity_query.csv $P/Structural_candidates.csv topdiff > $P/structural-verified.txt 2> /dev/null
+./resources/query_validate $P/trees.bracket $P/dissimilarity_query.csv $P/Sed_candidates.csv topdiff > $P/sed-verified.txt 2> /dev/null
