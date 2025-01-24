8ab1dec46848fdbece3fd1fec8c0353eb0a24fb0
Lukas Moravec <sydnexpklover@gmail.com>
2025-01-24 15:40:47 +0100

    Generated datasets comparison

--- dissimilarity_run.sh
+++ dissimilarity_run.sh
@@ -4,10 +4,10 @@ DS=$1
 LBLS=$2
 BASE="${3:-50}"
 echo $BASE;
-P=resources/workloads/generated/base-${BASE}/${DS}
+P=resources/workloads/generated-10-20/base-${BASE}/${DS}
 mkdir -p $P
 
-python3 tree_generator.py -T 1000 -D $LBLS -S 0.5 -M 95,105 -B $BASE -X 0.5 -A 30,50 > $P/trees.bracket
+python3 tree_generator.py -T 1000 -D $LBLS -S 0.5 -M 95,105 -B $BASE -X 0.5 -A 10,20 > $P/trees.bracket
 ./target/release/tree-statistics -d $P/trees.bracket statistics | tail -n 2 > $P/collection.csv
 python3  dissimilarity_query_gen.py -F $P/trees.bracket
 
