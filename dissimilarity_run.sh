5eeac935280b84c6feab44dc48719cf8f3a8ba12
Lukas Moravec <sydnexpklover@gmail.com>
2025-02-24 13:43:05 +0100

    Unified results verification

--- dissimilarity_run.sh
+++ dissimilarity_run.sh
@@ -18,14 +18,15 @@ echo $P;
 # ./target/release/tree-statistics --quiet -d $P/trees.bracket lower-bound -q $P/dissimilarity_query.csv --output $P sed | tail -n 18 > $P/query_times.txt
 # cat $P/query_times.txt
 # ./resources/query_validate $P/trees.bracket $P/dissimilarity_query.csv $P/Sed_candidates.csv topdiff > $P/dissimilarity-verified.txt 2> /dev/null
-./resources/ted-ds-dist $P trees.bracket 90
-./create_query_sample.py $P/distances-tjoin.txt $P/trees.bracket > $P/1query.csv
+# ./resources/ted-ds-dist $P trees.bracket 90
+# ./create_query_sample.py $P/distances-tjoin.txt $P/trees.bracket > $P/1query.csv
 
 QUERY_RESULT=$P/query-1
 mkdir -p $QUERY_RESULT
 
-./target/release/tree-statistics --quiet -d $P/trees.bracket lower-bound -q $P/1query.csv --output $QUERY_RESULT | tail -n 18 > $QUERY_RESULT/query_times.txt
-
-./resources/query_validate $P/trees.bracket $P/1query.csv $QUERY_RESULT/Lblint_candidates.csv topdiff > $QUERY_RESULT/lblint-verified.txt 2> /dev/null
-./resources/query_validate $P/trees.bracket $P/1query.csv $QUERY_RESULT/Sed_candidates.csv topdiff > $QUERY_RESULT/sed-verified.txt 2> /dev/null
-./resources/query_validate $P/trees.bracket $P/1query.csv $QUERY_RESULT/Structural_candidates.csv topdiff > $QUERY_RESULT/structural-verified.txt 2> /dev/null
+for it in {1..3}; do
+    ./target/release/tree-statistics --quiet -d $P/trees.bracket lower-bound -q $P/1query.csv --output $QUERY_RESULT | tail -n 18 > $QUERY_RESULT/query_times_${it}.txt
+done
+./resources/query_validate_3 $P/trees.bracket $P/1query.csv $QUERY_RESULT/Lblint_candidates.csv $QUERY_RESULT/Sed_candidates.csv $QUERY_RESULT/Structural_candidates.csv > $QUERY_RESULT/verified-all.txt 2> /dev/null
+# ./resources/query_validate $P/trees.bracket $P/1query.csv $QUERY_RESULT/Sed_candidates.csv topdiff > $QUERY_RESULT/sed-verified.txt 2> /dev/null
+# ./resources/query_validate $P/trees.bracket $P/1query.csv $QUERY_RESULT/Structural_candidates.csv topdiff > $QUERY_RESULT/structural-verified.txt 2> /dev/null
